7491459d57b520f90fde74354c9b637e2eb885ea
chigozie18 <chigozie.asikaburu@gmail.com>
2020-03-21 22:42:33 -0400

    Adds demo files for 3/20.

--- cloudsim-plus-examples/src/main/java/org/cloudsimplus/examples/research/FirstBasicScenario.java
+++ cloudsim-plus-examples/src/main/java/org/cloudsimplus/examples/research/FirstBasicScenario.java
@@ -54,7 +54,7 @@ import java.util.List;
  *
  * @author chigozieasikaburu
  */
-public class FirstAndBasicScenario {
+public class FirstBasicScenario {
 	
 	private static final int HOSTS = 1;  // number of hosts for each datacenter 
 	
@@ -80,10 +80,10 @@ public class FirstAndBasicScenario {
     private Datacenter datacenter0, datacenter1;
 
     public static void main(String[] args) {
-        new FirstAndBasicScenario();
+        new FirstBasicScenario();
     }
 
-    private FirstAndBasicScenario() {
+    private FirstBasicScenario() {
         /*Enables just some level of log messages.
           Make sure to import org.cloudsimplus.util.Log;*/
         //Log.setLevel(ch.qos.logback.classic.Level.WARN);
